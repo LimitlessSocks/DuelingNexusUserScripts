fcf3cea4246d62f8bbc9d497bcdc6b66ff1afdd2
LimitlessSocks <socktheringleader@gmail.com>
2020-02-15 03:35:33 -0500

    fix notifications loading weirdly on some pages

--- beta/Notifications/nexus-notifications.user.js
+++ beta/Notifications/nexus-notifications.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dueling Nexus Notification System
 // @namespace    https://duelingnexus.com/
-// @version      0.2
+// @version      0.2.2
 // @description  Gives desktop notifications for joining matches.
 // @author       Sock#3222
 // @grant        none
@@ -16,7 +16,11 @@
 // @include      https://duelingnexus.com/leaderboard
 // @include      https://duelingnexus.com/login
 // @include      https://duelingnexus.com/game/*
+// @updateURL    https://github.com/LimitlessSocks/DuelingNexusUserScripts/raw/master/beta/Notifications/nexus-notifications.user.js
+// @downloadURL  https://github.com/LimitlessSocks/DuelingNexusUserScripts/raw/master/beta/Notifications/nexus-notifications.user.js
 // ==/UserScript==
+
+console.info = console.info || console.log;
 
 let waitFrame = function () {
     return new Promise(resolve => {
@@ -47,29 +51,71 @@ let formatTime = function(time) {
     return result;
 };
 
+const NOTIFICATION_TIMEOUT = 4000;
 const loadNotifications = function () {
-    /* duel ready notifications */
     launchDuelReadyNotifications();
     
+    if(typeof Game !== "undefined" && Game) {
+        launchLobbyNotifications();
+    }
+};
+
+const augmentFunction = function (src, base, accessory, fn, opts) {
+    let oldFunction = src[base];
     
-    /* lobby notifications */
-    // NOTE: Might break.
-    window.zd = function zd(a) {
-        B[a.position] = {
-            name: a.name,
-            na: Number(a.avatar),
-            ba: a.customAvatarPath,
-            oa: a.customSleevePath,
-            ready: false
-        };
-        $("#game-room-player" + (a.position + 1) + "-username").text(a.name);
-        if(a.name !== Ib) {
-            let message = "Player \"" + a.name + "\" has joined the lobby.";
-            let notif = new Notification(message);
-            setTimeout(notif.close.bind(notif), 4000);
-        }
-        Ke();
+    opts = opts || { before: false };
+    
+    src[base] = function (...args) {
+        let [first, second] = opts.before ? [fn, oldFunction] : [oldFunction, fn];
+        first(...args);
+        second(...args);
     };
+    
+    if(src[accessory]) {
+        src[accessory] = src[base];
+    }
+    
+    return oldFunction;
+};
+
+const notifyMessage = function (message, timeout = NOTIFICATION_TIMEOUT) {
+    let notif = new Notification(message);
+    setTimeout(notif.close.bind(notif), timeout);
+};
+
+const launchLobbyNotifications = function () {
+    let playersCopy;
+    let myPosition;
+    
+    augmentFunction(Game, "onRoomPlayerJoined", "RoomPlayerJoined", function (a) {
+        console.log(">>> PLAYER JOIN <<<");
+        if(a.name !== Game.username) {
+            notifyMessage(`Player "${a.name}" has joined the lobby.`);
+        }
+        else if(!myPosition) {
+            myPosition = a;
+            console.log("MYPOS", myPosition);
+        }
+    }, {
+        before: false,
+    });
+    
+    augmentFunction(Game, "onRoomPlayerLeft", "RoomPlayerLeft", function (a) {
+        let oldPlayer = Game.players[a.position];
+        if(!oldPlayer) {
+            
+        }
+        console.log(">>> PLAYER LEFT <<<");
+        let kind = a.toObserver ? "moved to spectator" : "was kicked from the lobby";
+        
+        console.log("LEFT", a, oldPlayer, playersCopy);
+        
+        if(!a.toObserver || oldPlayer.name !== Game.username) {
+            // notifyMessage(`Player "${oldPlayer.name}" ${kind}.`);
+        }
+    }, {
+        before: true,
+    });
 };
 
 const launchDuelReadyNotifications = function () {
@@ -87,10 +133,7 @@ const launchDuelReadyNotifications = function () {
             launchDuelReadyNotifications();
         });
     });
-    
 };
-
-console.info = console.info || console.log;
 
 (function () {
     if(!("Notification" in window)) {
