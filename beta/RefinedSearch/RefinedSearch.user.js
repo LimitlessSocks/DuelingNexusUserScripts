d3be3d80490ccaca6196e2257f6bc3a48eead420
LimitlessSocks <socktheringleader@gmail.com>
2020-04-19 03:10:13 -0400

    oops, forgot to update teh version

--- beta/RefinedSearch/RefinedSearch.user.js
+++ beta/RefinedSearch/RefinedSearch.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DuelingNexus Deck Editor Revamp
 // @namespace    https://duelingnexus.com/
-// @version      0.19.8
+// @version      0.19.9
 // @description  Revamps the deck editor search feature.
 // @author       Sock#3222
 // @grant        none
