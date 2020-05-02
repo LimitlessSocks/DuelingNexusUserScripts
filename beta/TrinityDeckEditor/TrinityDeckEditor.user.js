2a9bf9a3c71e25fb5fbae92d98697c2f0ab7cbc6
LimitlessSocks <socktheringleader@gmail.com>
2020-05-02 14:36:27 -0400

    update to v.0.4.2

--- beta/TrinityDeckEditor/TrinityDeckEditor.user.js
+++ beta/TrinityDeckEditor/TrinityDeckEditor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dueling Nexus Trinity Deck Editor
 // @namespace    https://duelingnexus.com/
-// @version      0.4.1
+// @version      0.4.2
 // @description  Revamps the deck editor search feature.
 // @author       Sock#3222
 // @grant        none
