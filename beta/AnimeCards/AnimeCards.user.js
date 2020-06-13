9ccf38ecdd010350a47d31dd27daa27891570855
LimitlessSocks <socktheringleader@gmail.com>
2020-06-12 23:13:10 -0400

    add anime cards extension

---
+++ beta/AnimeCards/AnimeCards.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         Dueling Nexus Anime Cards Extension
+// @namespace    https://duelingnexus.com/
+// @description  Reskins the existing Dueling Nexus cards with Anime versions.
+// @author       Sock#3222
+// @version      0.1
+// @match        https://duelingnexus.com/game/*
+// @match        https://duelingnexus.com/editor/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/LimitlessSocks/DuelingNexusUserScripts/master/beta/AnimeCards/AnimeCards.user.js
+// @downloadURL  https://raw.githubusercontent.com/LimitlessSocks/DuelingNexusUserScripts/master/beta/AnimeCards/AnimeCards.user.js
+// ==/UserScript==
+
+(function() {
+    window.usingAnimeCards = true;
+
+    //https://cdn.jsdelivr.net/gh/ElvinaOlacarynWorld/OlacarynWorld@d079300/18743376.jpg
+    const getAnimeAsset = (id) => "https://raw.githubusercontent.com/LimitlessSocks/UnendingLegacy/master/small/" + id + ".jpg";
+
+    let oldGetPicture = Engine.getCardPicturePath;
+    Engine.getCardPicturePath = function (id, usingAnime = window.usingAnimeCards) {
+        if(usingAnime && id !== 0) {
+            return getAnimeAsset(id);
+        }
+        else {
+            return oldGetPicture(id);
+        }
+    }
+
+    let oldSetCardImage = Engine.setCardImageElement;
+
+    Engine.setCardImageElement = function (a, b, useAnime = window.usingAnimeCards) {
+        a.off("error");
+        a.attr("src", Engine.getCardPicturePath(b, useAnime));
+        if (0 < b) a.one("error", function() {
+            if(useAnime) {
+                Engine.setCardImageElement(a, b, false);
+            }
+            else {
+                $(this).attr("src", Engine.getCardPicturePath(-1));
+            }
+        })
+    };
+})();
